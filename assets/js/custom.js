0f44d9e1cea18f3d03c8132284f44c9b134aa1f0
dhanush <dhanush.dhoni007@gmail.com>
2025-11-11 14:25:56 +0530

    change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -198,3 +198,12 @@
 
   
 })(jQuery);
+
+
+  const faqs = document.querySelectorAll(".faq-item");
+  faqs.forEach(faq => {
+    faq.querySelector(".faq-question").addEventListener("click", () => {
+      faq.classList.toggle("active");
+    });
+  });
+
